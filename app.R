a34f4e2092cc20521ceccd209e17cf6c34a3b7f8
Arya Zandvakili <28754724+aryazand@users.noreply.github.com>
2020-04-25 20:50:01 -0400

    Allow function to analyze data from country, US state, and US county

--- app.R
+++ app.R
@@ -15,7 +15,8 @@ library(ggpubr)
 # Load Data
 # ****************
 
-NYT_countydata <- read_csv("./data/NYT_us-counties.csv", col_types = "Dcccnn")
+NYT_countydata <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", col_types = "Dcccnn")
+NYT_statedata <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", col_types = "Dccnn")
 
 # *****************
 # Transform Data
@@ -23,20 +24,23 @@ NYT_countydata <- read_csv("./data/NYT_us-counties.csv", col_types = "Dcccnn")
 
 # Create a unique idea for each county based on names
 NYT_countydata <- NYT_countydata %>% 
-    mutate(countystate = paste(county, state, sep=", "))
+    mutate(label = paste(county, state, sep=", "))
+
+NYT_statedata <- NYT_statedata %>% 
+    mutate(label = state)
 
 # Calculate new cases
-NYT_countydata <- NYT_countydata %>%
-    group_by(county, state, fips) %>%
-    arrange(date) %>%
-    mutate(new.cases = cases - c(0, cases[-length(cases)])) %>%
-    ungroup()
+newcases_helperfunction = function(data) {
+    data <- data %>%
+        group_by_at(setdiff(names(data), c("date", "cases", "deaths"))) %>%
+        arrange(date) %>%
+        mutate(new.cases = cases - c(0, cases[-length(cases)])) %>%
+        ungroup()
+    return(data)
+}
 
-# ***************
-# Create labels for ui from data
-# ***************
-
-county.states <- paste(NYT_countydata$county, NYT_countydata$state, sep=", ") %>% unique()
+NYT_countydata <- newcases_helperfunction(NYT_countydata)
+NYT_statedata <- newcases_helperfunction(NYT_statedata)
 
 # *************
 # User Interface 
@@ -51,9 +55,15 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            selectInput(inputId = "county",
-                        label = "Search & Select County (multiple selection allowed):",
-                        choices = c("Choose one" = "", county.states),
+            radioButtons(inputId = "datasource",
+                         label = "Select type geographic level: ",
+                         choices = c("State", "County"),
+                         inline = T,
+                         selected = "State"),
+            
+            selectInput(inputId = "geographic_location",
+                        label = "Search & Select Geographic Location (multiple selection allowed):",
+                        choices = c("Select regions" = "", NYT_statedata$label),
                         multiple = T
                         ),
             
@@ -69,14 +79,14 @@ ui <- fluidPage(
             radioButtons(inputId = "smoothing_kernal",
                          label = "Select a smoothing kernal for smoothing new cases per each day:",
                          choices = c("box", "normal"),
-                         selected = "box"),
+                         inline = T),
             
             sliderInput(inputId = "smoothing_bandwidth",
                         label = "Smoothing Bandwidth:",
                         min = 1, max = 5, value = 3, step = 1)
         ),
 
-        # Show a plot of the generated distribution
+        # Plots
         mainPanel(
            plotOutput("Plot_TotalCases"),
            plotOutput("Plot_NewCases"), 
@@ -86,24 +96,42 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, clientData, session) {
+    
+    observe({
+        if(input$datasource == "State") {
+            data <- NYT_statedata
+        } else {
+            data <- NYT_countydata   
+        }
+
+        updateSelectInput(session, "geographic_location",
+                           label = "Search & Select Geographic Location (multiple selection allowed):",
+                           choices = c("Select regions" = "", unique(data$label)))
+    })
     
     # Subset data to only the county of interest
     data = reactive({
-        NYT_countydata %>%
+        if(input$datasource == "State") {
+            data <- NYT_statedata
+        } else {
+            data <- NYT_countydata   
+        }
+        
+        data %>%
             arrange(date) %>%
-            group_by(county, state, fips) %>%
+            group_by_at(setdiff(names(data), c("date", "cases", "deaths", "new.cases"))) %>%
             mutate(new.cases_smoothed = ksmooth(date, new.cases, kernel = input$smoothing_kernal, bandwidth = input$smoothing_bandwidth, n.points = n())$y) %>%
             mutate(denominator = head(c(rep(NA, input$reference_date*-1), new.cases_smoothed), input$reference_date)) %>%
             mutate(denominator = replace(denominator, denominator == 0, 1)) %>%
             ungroup() %>%
             mutate(Rs = new.cases_smoothed/denominator) %>%
-            filter(countystate %in% input$county, date >= as.Date(input$dateRange[1]), date <= as.Date(input$dateRange[2]))
+            filter(label %in% input$geographic_location, date >= as.Date(input$dateRange[1]), date <= as.Date(input$dateRange[2]))
     })
     
     output$Plot_TotalCases <- renderPlot({
-        ggplot(data(), aes(date, cases, group = countystate)) + 
-            geom_line(aes(color = countystate)) +
+        ggplot(data(), aes(date, cases, group = label)) + 
+            geom_line(aes(color = label)) +
             labs(title = "Total Cases") + 
             theme_pubr() + 
             theme(legend.title = element_blank())
@@ -111,16 +139,16 @@ server <- function(input, output) {
     
     output$Plot_NewCases <- renderPlot({
         ggplot(data()) + 
-            geom_line(aes(date, new.cases, group = countystate, color = countystate), alpha=0.75) +
-            geom_line(aes(date, new.cases_smoothed, group = countystate, color = countystate), alpha=1, size = 1.5) + 
+            geom_line(aes(date, new.cases, group = label, color = label), alpha=0.75) +
+            geom_line(aes(date, new.cases_smoothed, group = label, color = label), alpha=1, size = 1.5) + 
             labs(title = "New Cases") + 
             theme_pubr() + 
             theme(legend.title = element_blank())
     })
     
     output$Plot_Rstat <- renderPlot({
-        ggplot(data(), aes(date, Rs, group = countystate)) + 
-            geom_line(aes(color = countystate)) +
+        ggplot(data(), aes(date, Rs, group = label)) + 
+            geom_line(aes(color = label)) +
             labs(title = "Simplified R estimate") +
             theme_pubr() + 
             theme(legend.title = element_blank())
