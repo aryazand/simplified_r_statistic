3b26b4e5e7bbb067c378f28c21ea3001f4d96682
Arya Zandvakili <28754724+aryazand@users.noreply.github.com>
2020-05-06 13:05:21 -0400

    Added in simple description/disclaimer at top. Improved the labels on input controls.

--- app.R
+++ app.R
@@ -66,23 +66,32 @@ ui <- fluidPage(
     # *************
     
     fluidRow(
-        # Have a tab for each plot
+        # 3 Tabs, one for total cases, new cases, and R calculations 
+        # Each tab has a plot and an information bar to the right
+        
         tabsetPanel(type="tabs",
                     tabPanel("Total Cases", 
                              column(10, plotOutput("Plot_TotalCases", 
-                                                   hover = hoverOpts(id ="TotalCases.hover", delay=50))#,
-                                                   # dblclick = "TotalCases.dblclk",
-                                                   # brush = brushOpts(id = "TotalCases.brush", resetOnNew = TRUE)
-                                    ),
+                                                   hover = hoverOpts(id ="TotalCases.hover", delay=50),
+                                                   dblclick = "TotalCases.dblclk",
+                                                   brush = brushOpts(id = "TotalCases.brush", resetOnNew = TRUE)
+                                    )),
                              column(2, htmlOutput("TotalCases.info"))
-                             
                              ),
                     tabPanel("New Cases", 
-                             column(10, plotOutput("Plot_NewCases", hover = hoverOpts(id ="NewCases.hover", delay=50))),
+                             column(10, plotOutput("Plot_NewCases", 
+                                                   hover = hoverOpts(id ="NewCases.hover", delay=50),
+                                                   dblclick = "NewCases.dblclk",
+                                                   brush = brushOpts(id = "NewCases.brush", resetOnNew = TRUE)
+                             )),
                              column(2, htmlOutput("NewCases.info"))
                              ), 
                     tabPanel("R", 
-                             column(10, plotOutput("Plot_Rstat", hover = hoverOpts(id ="R.hover", delay = 50))),
+                             column(10, plotOutput("Plot_Rstat", 
+                                                   hover = hoverOpts(id ="R.hover", delay = 50),
+                                                   dblclick = "R.dblclk",
+                                                   brush = brushOpts(id = "R.brush", resetOnNew = TRUE)
+                             )),
                              column(2, htmlOutput("R.info"))
                              )    
         )
@@ -309,79 +318,50 @@ server <- function(input, output, clientData, session) {
         # Code from: https://shiny.rstudio.com/gallery/plot-interaction-zoom.html
         
         if (!is.null(brush)) {
-            ranges$x <- c(brush$xmin, brush$xmax)
+            ranges$x <- as.Date(c(brush$xmin, brush$xmax), origin="1970-01-01")
             ranges$y <- c(brush$ymin, brush$ymax)
             
         } else {
-            ranges$x <- NULL
+            ranges$x <- c(as.Date(input$dateRange[1]),  as.Date(input$dateRange[2]))
             ranges$y <- NULL
         }
     }
     
     # Total Cases
     TotalCases.ranges <- reactiveValues(x = NULL, y = NULL)
-    output$Plot_TotalCases <- renderPlot({
-        
-        if(is.null(TotalCases.ranges)) { 
-            dummyTable = data() %>% filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2]))
-            max_y_value = max(dummyTable$total_cases, na.rm = T)*1.1
-            
-            ggplot(data(), aes(date, total_cases, group = region)) +
-                geom_line(aes(color = region)) +
-                labs(y = 'Total Cases') +
-                scale_x_date(date_breaks = '1 week', date_labels = '%b-%d',
-                             limits=c(as.Date(input$dateRange[1]),  as.Date(input$dateRange[2]))) +
-                scale_y_continuous(labels = function(x) format(x, scientific = F),
-                                   limits = c(0, max_y_value)) + 
-                custom_plot_theme()
-        } else {
-            ggplot(data(), aes(date, total_cases, group = region)) +
-                geom_line(aes(color = region)) +
-                labs(y = 'Total Cases') +
-                scale_x_date(date_breaks = '1 week', date_labels = '%b-%d') + 
-                scale_y_continuous(labels = function(x) format(x, scientific = F)) + 
-                coord_cartesian(xlim = Plot_TotalCases$x, ylim = Plot_TotalCases$y, expand = FALSE)
-                custom_plot_theme()
-        }
+    Plot_TotalCases <- reactive({
+        ggplot(data(), aes(date, total_cases, group = region)) +
+            geom_line(aes(color = region)) +
+            labs(y = 'Total Cases') +
+            scale_x_date(date_breaks = '1 week', date_labels = '%b-%d') +
+            scale_y_continuous(labels = function(x) format(x, scientific = F)) +
+            coord_cartesian(xlim = TotalCases.ranges$x, ylim = TotalCases.ranges$y, expand = FALSE) + 
+            custom_plot_theme()
+    })
+    output$Plot_TotalCases <- renderPlot({Plot_TotalCases()})
+    observeEvent(input$TotalCases.dblclk, {
+        function_to_zoom(input$TotalCases.brush, TotalCases.ranges)
     })
     
-    # #observeEvent(input$TotalCases.dblclk, {
-    #     brush = input$TotalCases.brush
-    #     if (!is.null(brush)) {
-    #         TotalCases.ranges$x <- c(brush$xmin, brush$xmax)
-    #         TotalCases.ranges$y <- c(brush$ymin, brush$ymax)
-    #         
-    #     } else {
-    #         TotalCases.ranges$x <- NULL
-    #         TotalCases.ranges$y <- NULL
-    #     }
-    #   #  function_to_zoom(input$TotalCases.brush, TotalCases.ranges)
-    # })
-    
-    
-
     # New Cases
-    output$Plot_NewCases <- renderPlot({
-        dummyTable = data() %>% filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2]))
-        max_y_value = max(dummyTable$new_cases, na.rm = T)*1.1
-        
+    NewCases.ranges <- reactiveValues(x = NULL, y = NULL)
+    Plot_NewCases <- reactive({(
         ggplot(data()) + 
             geom_line(aes(date, new_cases, group = region, color = region), alpha=0.75) +
             geom_line(aes(date, new_cases.smoothed, group = region, color = region), alpha=1, size = 1.5) + 
             labs(y = "New Cases") +
-            scale_x_date(date_breaks = '1 week', date_labels = '%b-%d',
-                         limits=c( as.Date(input$dateRange[1]),  as.Date(input$dateRange[2]))) +
-            ylim(0, max_y_value) + 
-            custom_plot_theme() 
+            scale_x_date(date_breaks = '1 week', date_labels = '%b-%d') + 
+            coord_cartesian(xlim = NewCases.ranges$x, ylim = NewCases.ranges$y, expand = FALSE) + 
+            custom_plot_theme()
+    )})
+    output$Plot_NewCases <- renderPlot({Plot_NewCases()})
+    observeEvent(input$NewCases.dblclk, {
+        function_to_zoom(input$NewCases.brush, NewCases.ranges)
     })
     
     # R estimate
-    
-    output$Plot_Rstat <- renderPlot({
-        dummyTable = data() %>% filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2]))
-        dummyTable2 = data.epiestim() %>% filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2]))
-        max_y_value = max(dummyTable$Rs, dummyTable2$`Quantile.0.975(R)`,na.rm=T)*1.1
-        
+    R.ranges <- reactiveValues(x = NULL, y = NULL)
+    Plot_Rstat <- reactive({
         ggplot(data()) + 
             geom_line(aes(x = date, y = Rs, color = region, group = region)) +
             geom_ribbon(data = data.epiestim(), aes(x = date,  ymin=`Quantile.0.025(R)`,ymax=`Quantile.0.975(R)`, group = region, fill = region), alpha=0.15)+
@@ -391,54 +371,96 @@ server <- function(input, output, clientData, session) {
             geom_hline(yintercept = 1, linetype = 2) + 
             labs(subtitle = "dotted lines give R estimation from EpiEstim with a 95% CI") +
             labs(y = "R estimate") +
-            scale_x_date(date_breaks = '1 week', date_labels = '%b-%d',
-                         limits=c(as.Date(input$dateRange[1]),  as.Date(input$dateRange[2]))) +
-            ylim(0, max_y_value) + 
+            scale_x_date(date_breaks = '1 week', date_labels = '%b-%d') + 
+            coord_cartesian(xlim = R.ranges$x, ylim = R.ranges$y, expand = FALSE) + 
             custom_plot_theme()
     })
-    
+    output$Plot_Rstat <- renderPlot({
+        Plot_Rstat()
+    })
+    observeEvent(input$R.dblclk, {
+        function_to_zoom(input$R.brush, R.ranges)
+    })
     
     # *************
     # Hover Info
     # based on: https://stackoverflow.com/questions/34370780/r-interactive-plot-show-vertical-line-on-hover
     # *************
     
+    info_on_hover_func <- function(plot_obj, hover_obj, ranges, threshold = 20, maxpoints = 1) {
+        
+        # Create a dummy table based on the x & y ranges of the plot
+        # Using nearPoints with this table will allow us to find the x & y values of the
+        # mouse no matter where the mouse is on the plot
+        
+        data = plot_obj$data
+        
+        # Get mapped variables
+        # based on https://cran.r-project.org/web/packages/gginnards/vignettes/user-guide-2.html
+        if(length(plot_obj$layers) == 1) {
+            mapped.vars <- gsub("[~*\\%^]", " ", as.character(plot_obj$mapping)) %>%
+                str_split(boundary("word")) %>%
+                unlist() %>% 
+                list()
+ 
+        } else {
+            mapped.vars <- map(plot_obj$layers, function(x) gsub("[~*\\%^]", "", as.character(x$mapping))) 
+        }
+    
+        n = 1000
+        
+        if(!is.null(ranges$x) & !is.null(ranges$y)) { 
+            dates = seq(as.Date(ranges$x[1]), as.Date(ranges$x[2]), length.out = n)
+            y_vals = seq(ranges$y[1], ranges$y[2], length.out = n)
+        } else {
+            dates = seq(as.Date(input$dateRange[1]), as.Date(input$dateRange[2]), length.out = n)
+            
+            mapped.vars.y = map_chr(mapped.vars, function(x) x[2])
+            mapped.vars.y_max = map_dbl(mapped.vars.y, function(x) max(data[[x]], na.rm=T))
+            max_y = max(mapped.vars.y_max, na.rm=T)
+            y_vals = seq(-1, max_y, length.out = n)
+        }
+        
+        dummytable = data.frame(
+            date = rep(dates, n),
+            y_vals = rep(y_vals, each=length(dates))
+        )
+        colnames(dummytable)[2] = mapped.vars[[1]][2]
+        
+       
+        
+        nearPoint <- nearPoints(dummytable, hover_obj, threshold = threshold, maxpoints = maxpoints)
+
+        return(nearPoint)
+    }
+    
+    info_display_func <- function(data, parameters_to_display) {
+        
+        # HTML to render in to a information about data
+        # helper function to display where mouse is hovering
+        date_title = paste0("Date ", data$date[1], "<br/>")
+        create_str_func = function(df.row) {
+            title = df.row["region"]
+            df.row = as.character(df.row[!(names(df.row) %in% c("region", "date"))])
+            info = paste0(paste0(parameters_to_display, ".....", df.row, "<br/>"), collapse="")
+            info = paste0("<strong>", title, "</strong>", "<br/>", info, "<br/>")
+            return(info)
+        }
+        
+        output_strings = apply(data, 1, create_str_func)
+        output_strings = paste0(c(date_title, output_strings), collapse="")
+        return(output_strings)
+    }
+    
     output$TotalCases.info <- renderUI({
         data = data()
 
         if(nrow(data) > 1 & !is.null(input$TotalCases.hover)) {
-            max_y_value = data %>% 
-                filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2])) %>%
-                .$total_cases %>%
-                max(na.rm=T)
-            
-            dates = seq(as.Date(input$dateRange[1])-5, as.Date(input$dateRange[2])+5, "days")
-            cases = seq(-1, max_y_value*1.1, length.out = 1000)
-
-
-            dummytable = data.frame(
-                date = rep(dates, 1000),
-                total_cases = rep(cases, each=length(dates))
-            )
-
-            nearPoint <- nearPoints(dummytable, input$TotalCases.hover, threshold = 10, maxpoints = 1)
-
-            closedata = data() %>% filter(date == nearPoint$date) %>% select(date, region, total_cases)
-            
-            date_title = paste0("Date ", closedata$date[1], "<br/>")
-            parameters = c("Total Cases")
-            create_str_func = function(df.row) {
-                title = df.row["region"]
-                df.row = as.character(df.row[!(names(df.row) %in% c("region", "date"))])
-                info = paste0(paste0(parameters, ".....", df.row, "<br/>"), collapse="")
-                info = paste0("<strong>", title, "</strong>", "<br/>", info, "<br/>")
-                return(info)
-            }
-            
-            output_strings = apply(closedata, 1, create_str_func)
-            
-            HTML(paste0(c(date_title, output_strings), collapse=""))
-            
+            nearPoint <- info_on_hover_func(Plot_TotalCases(), input$TotalCases.hover, TotalCases.ranges)
+            nearPoint$date = as.character(nearPoint$date)
+            data.subset = data %>% mutate(date = as.character(date)) %>% filter(date == nearPoint$date) %>% select(date, region, total_cases) 
+            output_string = info_display_func(data.subset, c("Total Cases"))
+            HTML(output_string)
         } else {
          NULL
         }
@@ -448,37 +470,12 @@ server <- function(input, output, clientData, session) {
         data = data()
         
         if(nrow(data) > 1 & !is.null(input$NewCases.hover)) {
-            max_y_value = data %>% 
-                filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2])) %>%
-                .$new_cases %>%
-                max(na.rm=T)
-            
-            dates = seq(as.Date(input$dateRange[1])-5, as.Date(input$dateRange[2])+5, "days")
-            cases = seq(-1, max_y_value*1.1, length.out = 1000)
-            
-            
-            dummytable = data.frame(
-                date = rep(dates, 1000),
-                new_cases = rep(cases, each=length(dates))
-            )
-            
-            nearPoint <- nearPoints(dummytable, input$NewCases.hover, threshold = 10, maxpoints = 1)
-            
-            closedata = data() %>% filter(date == nearPoint$date) %>% select(date, region, new_cases, new_cases.smoothed)
-            
-            date_title = paste0("Date ", closedata$date[1], "<br/>")
-            parameters = c("Raw New Cases", "Smoothed New Cases")
-            create_str_func = function(df.row) {
-                title = df.row["region"]
-                df.row = as.character(df.row[!(names(df.row) %in% c("region", "date"))])
-                info = paste0(paste0(parameters, ".....", df.row, "<br/>"), collapse="")
-                info = paste0("<strong>", title, "</strong>", "<br/>", info, "<br/>")
-                return(info)
-            }
-            
-            output_strings = apply(closedata, 1, create_str_func)
-            
-            HTML(paste0(c(date_title, output_strings), collapse=""))
+                nearPoint <- info_on_hover_func(Plot_NewCases(), input$NewCases.hover, NewCases.ranges)
+                nearPoint$date = as.character(nearPoint$date)
+                data.subset = data %>% mutate(date = as.character(date)) %>% 
+                    filter(date == nearPoint$date) %>% select(date, region, new_cases, new_cases.smoothed)
+                output_string = info_display_func(data.subset, c("New Cases Raw", "New Cases Smoothed"))
+                HTML(output_string)
         } else {
             NULL
         }
@@ -489,53 +486,23 @@ server <- function(input, output, clientData, session) {
         data.epiestim = data.epiestim()
         
         if(nrow(data) > 1 & !is.null(input$R.hover)) {
-            max_y_value_1 = data %>% 
-                filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2])) %>%
-                .$Rs %>%
-                max(na.rm=T)
-            
-            max_y_value_2 = data.epiestim %>% 
-                filter(date >= as.Date(input$dateRange[1]) & date <= as.Date(input$dateRange[2])) %>%
-                .$`Quantile.0.975(R)` %>%
-                max(na.rm=T)
-            
-            max_y_value = max(max_y_value_1, max_y_value_2, na.rm=T)
-            
-            dates = seq(as.Date(input$dateRange[1])-5, as.Date(input$dateRange[2])+5, "days")
-            cases = seq(-1, max_y_value*1.1, length.out = 1000)
-            
-            
-            dummytable = data.frame(
-                date = rep(dates, 1000),
-                Rs = rep(cases, each=length(dates))
-            )
-            
-            nearPoint <- nearPoints(dummytable, input$R.hover, threshold = 10, maxpoints = 1)
+            nearPoint <- info_on_hover_func(Plot_Rstat(), input$R.hover, R.ranges)
+            nearPoint$date = as.character(nearPoint$date)
+            data1 = data %>% mutate(date = as.character(date)) %>% 
+                        filter(date == nearPoint$date) %>%
+                        select(date, region, Rs) %>%
+                        mutate(Rs = round(Rs, 3))
+            data2 = data.epiestim %>% mutate(date = as.character(date)) %>% 
+                filter(date == nearPoint$date) %>%
+                select(c(date, region, `Mean(R)`, `Quantile.0.025(R)`, `Quantile.0.975(R)`)) %>%
+                mutate(`Mean(R)` = round(`Mean(R)`, 3),
+                       `Quantile.0.025(R)` = round(`Quantile.0.025(R)`,3),
+                       `Quantile.0.975(R)` = round(`Quantile.0.975(R)`,3))
+            data = full_join(data1, data2, by=c("date", "region"))# %>% unite("pastestring", -date, sep=", ")
+            output_string = info_display_func(data, c("Rs", "EpiEstim_R", "EpiEstim_R0.025", "EpiEstim_R0.975"))
+            HTML(output_string)
             
 
-            closedata = data %>% filter(date == nearPoint$date) %>% 
-                        select(date, region, Rs) %>% 
-                        mutate(Rs = round(Rs, 3))
-            closedata.epiestim = data.epiestim %>% filter(date == nearPoint$date) %>% 
-                select(c(date, region, `Mean(R)`, `Quantile.0.025(R)`, `Quantile.0.975(R)`)) %>% 
-                mutate(`Mean(R)` = round(`Mean(R)`, 3),
-                       `Quantile.0.025(R)` = round(`Quantile.0.025(R)`,3), 
-                       `Quantile.0.975(R)` = round(`Quantile.0.975(R)`,3))
-            closedata = full_join(closedata, closedata.epiestim, by=c("date", "region"))# %>% unite("pastestring", -date, sep=", ")
-            
-            date_title = paste0("Date ", closedata$date[1], "<br/>")
-            parameters = c("Rs", "EpiEstim_R", "EpiEstim_R0.025", "EpiEstim_R0.975")
-            create_str_func = function(df.row) {
-                title = df.row["region"]
-                df.row = as.character(df.row[!(names(df.row) %in% c("region", "date"))])
-                info = paste0(paste0(parameters, ".....", df.row, "<br/>"), collapse="")
-                info = paste0("<strong>", title, "</strong>", "<br/>", info, "<br/>")
-                return(info)
-            }
-            
-            output_strings = apply(closedata, 1, create_str_func)
-            
-            HTML(paste0(c(date_title, output_strings), collapse=""))
         } else {
             NULL   
         }
