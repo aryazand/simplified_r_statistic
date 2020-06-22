3b56e2a7d82967ead02ff54cc5e18be92967348a
aryazand <28754724+aryazand@users.noreply.github.com>
2020-06-22 11:58:19 -0500

    filter out regions with minimal data

--- scripts/Generate_Data_For_Publication_Results.R
+++ scripts/Generate_Data_For_Publication_Results.R
@@ -36,6 +36,12 @@ parameter_combos = cross(parameter_list)
 #------------
 
 data = read_csv("./simplified_r_statistic/case_data.csv")
+data = data %>%
+  group_by(region, regionID, region_type, regionID_type) %>%
+  filter(n() > 20) %>%
+  filter(max(new_cases) > 10) %>%
+  ungroup()
+
 set.seed(19890616)
 data_1 = data %>% filter(region_type == "state") %>% filter(region %in% sample(unique(.$region), 10))
 data_2 = data %>% filter(region_type == "county") %>% filter(region %in% sample(unique(.$region), 10))
